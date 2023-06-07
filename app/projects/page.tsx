852f4ada11f73d4a8c18fee64ad257990b42af74
Zach McMullen <zachmcmullen04@gmail.com>
2023-06-06 19:30:09 -0700

    [Fix] expect error async server component

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -2,7 +2,6 @@ import ProjectList from "../components/ProjectList"
 
 export default function Projects() {
     return (
-        /* @ts-expect-error Async Server Component */
         <ProjectList/>
     )
 }
