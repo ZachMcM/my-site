a37a169d13e093bcba691091b559e5cc09796839
Zach McMullen <zachmcmullen04@gmail.com>
2023-06-17 16:18:55 -0700

    [Update] Adding Skill

--- app/components/LanguageIcon.tsx
+++ app/components/LanguageIcon.tsx
@@ -1,6 +1,6 @@
 import { IconType } from "react-icons";
 
-import { SiC, SiCplusplus, SiCsharp, SiCss3, SiExpress, SiGit, SiHtml5, SiJavascript, SiNextdotjs, SiNodedotjs, SiPostgresql, SiPrisma, SiPython, SiReact, SiTailwindcss, SiTypescript } from "react-icons/si"
+import { SiC, SiCplusplus, SiCsharp, SiCss3, SiExpress, SiGit, SiHtml5, SiJavascript, SiNextdotjs, SiNodedotjs, SiPostgresql, SiPrisma, SiPython, SiReact, SiTailwindcss, SiTypescript, SiSupabase } from "react-icons/si"
 import { FaJava } from "react-icons/fa";
 
 type LanguageIconMap = Record<string, IconType>;
@@ -22,7 +22,8 @@ const languageIconMap: LanguageIconMap = {
     'css': SiCss3,
     "html": SiHtml5,
     "prisma": SiPrisma,
-    "postgresql": SiPostgresql
+    "postgresql": SiPostgresql,
+    "supabase": SiSupabase
 }
 
 export default function( {language} : { language: string }) {
--- data/skills.ts
+++ data/skills.ts
@@ -10,5 +10,6 @@ export default [
     "Java",
     "Git",
     "Prisma",
-    "Postgresql"
+    "Postgresql",
+    "Supabase"
 ]
